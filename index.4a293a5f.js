1024b6f4392935c5fea81dce19b4fe939e43f9c4
MarcinNieradka <116951450+MarcinNieradka@users.noreply.github.com>
2023-10-18 16:48:56 +0000

    Deploying to gh-pages from @ MarcinNieradka/Project-IceCream@b31ab6d891b7533aec2166bc68e11d0170f79005 🚀

---
+++ index.4a293a5f.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector(".js-menu-container"),t=document.querySelector(".js-open-menu"),n=document.querySelector(".js-close-menu"),c=()=>{const n="true"===t.getAttribute("aria-expanded")||!1;t.setAttribute("aria-expanded",!n),e.classList.toggle("is-open")};t.addEventListener("click",c),n.addEventListener("click",c)})();
+//# sourceMappingURL=index.4a293a5f.js.map
